4fbaf00131d6882b0d51c0d15ae226fc71fef645
esterlilianlb <esterlilian2231@gmail.com>
2020-03-26 03:34:45 -0300

    integração do front com a api até o login

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -7,11 +7,13 @@ import logoImg from '../../assets/logo.svg';
 import './styles.css';
 
 export default function Profile() {
+  const ongName = localStorage.getItem('ongName');
+
   return(
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero"/>
-        <span>Bem-vinda APAD</span>
+        <span>Bem-vinda {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
@@ -21,48 +23,6 @@ export default function Profile() {
 
       <h1>Casos cadastrados</h1>
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO</strong>
-          <p>peixe banana bola gato</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO</strong>
-          <p>peixe banana bola gato</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO</strong>
-          <p>peixe banana bola gato</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
         <li>
           <strong>CASO:</strong>
           <p>Caso teste</p>
