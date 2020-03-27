714487fea9185cff6a2d7bb27b52d4d9a811c797
esterlilianlb <esterlilian2231@gmail.com>
2020-03-27 05:18:03 -0300

    param name adicionado ao incidentcontroller

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -11,7 +11,8 @@ module.exports = {
         .limit(5)
         .offset((page-1)*5)
         .select([
-            'incidents.*', 
+            'incidents.*',
+            'ongs.name',
             'ongs.email', 
             'ongs.whatsapp', 
             'ongs.city', 
